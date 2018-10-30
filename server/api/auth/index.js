b1b5f55801bfded93e4042bdd02514f05d35facd
Christopher Franco <francotorium@gmail.com>
2018-10-30 01:32:35 -0700

    accounts can be created with a valid email and password and persisted to the database - but postgres errors out when you initially seed the database and then add a new account -- will need to resolve seeding in the future

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const validator = require('validator');
 const Account = require('../../db/account');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 router.get('/',(req,res) => {
     res.json({
@@ -17,24 +19,37 @@ function validatorAccount(account) {
      return validEmail && validPassword;
 }
 
-router.post('/signup', (req, res, next) => {
-    console.log('body',req.body);
-    const validAccount = validatorAccount(req.body);
-    if(validAccount){
-        Account.getOneByEmail(req.body.email.trim()).then(account => {
-            console.log('account', account);
-            if (!account) {
-                res.json({
-                    account,
-                    message:'🔥'
-                });
-            } else {
-                next(new Error('Email in use'));
-            }
-        });
-    } else {
-        next(new Error('Invalid Account'));
-    }
+router.post("/signup", (req, res, next) => {
+  console.log("body", req.body);
+  const validAccount = validatorAccount(req.body);
+  if (validAccount) {
+    Account.getOneByEmail(req.body.email.trim()).then(account => {
+      console.log("account", account);
+      if (!account) {
+        //technique #2 of bycrypt
+        bcrypt.hash(
+          req.body.password.trim(),
+          parseInt(process.env.SALT_ROUNDS),
+          function(err, hash) {
+            // Store hash in your password DB.
+            const account = {
+              email: req.body.email.trim(),
+              password: hash
+            };
+            Account.create(account).then(accountId => {
+              res.json({
+                message: "defintely some #️⃣"
+              });
+            });
+          }
+        );
+      } else {
+        next(new Error("Email in use"));
+      }
+    });
+  } else {
+    next(new Error("Invalid Account"));
+  }
 });
 
 module.exports = router;
