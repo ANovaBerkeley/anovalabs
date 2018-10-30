ec6d345700c32241f31c4cc020727fc8302c965d
Christopher Franco <francotorium@gmail.com>
2018-10-30 02:32:15 -0700

    removing the id field for accounts has resolved the primary key duplication

--- server/seedData/accounts.js
+++ server/seedData/accounts.js
@@ -1,26 +1,21 @@
 module.exports = [
   {
-    accountId: 1,
     email: "sam@domain.com",
     password: "123hash"
   },
   {
-    accountId: 2,
     email: "mas@domain.com",
     password: "456hash"
   },
   {
-    accountId: 3,
     email: "asm@domain.com",
     password: "789hash"
   },
   {
-    accountId: 4,
     email: "samsam@domain.com",
     password: "321hash"
   },
   {
-    accountId: 5,
     email: "samiam@domain.com",
     password: "654hash"
   }
