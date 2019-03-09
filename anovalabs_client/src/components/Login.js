18ee5a39906a57c89eb051d66f475bb6e807eceb
pratibha99 <pratibha99@berkeley.edu>
2019-03-08 17:21:06 -0800

    started login frontend

--- anovalabs_client/src/components/Login.js
+++ anovalabs_client/src/components/Login.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+//import styles from "./LoginStyles";
+
+import './Login.css';
+
+// const styles = {
+//     container: {
+//       backgroundColor: "#E6F4FC",
+//       width: '100%',
+//       height: '100%',
+//       flex: 1,
+//       flexDirection: 'column',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//
+//
+//     },
+//     loginbox: {
+//       margin: 'auto',
+//       width: '420px',
+//       height: '521px',
+//       backgroundColor: 'rgba(247, 247, 247, 0.86)',
+//
+//     }
+//
+// }
+
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -37,31 +63,37 @@ class Login extends Component {
       });
   }
 
+
+
   render() {
     return (
-      <div>
-        <form onSubmit={this._submit}>
-          <div>
-            <label>email</label>
-            <input
-              type="text"
-              name="email"
-              onChange={this._change}
-              value={this.state.email}
-            />
-          </div>
-          <div>
-            <label>password</label>
-            <input
-              type="password"
-              name="password"
-              onChange={this._change}
-              value={this.state.password}
-            />
-            <div>{this.state.errorMsg}</div>
-          </div>
-          <input type="submit" value="submit" />
-        </form>
+
+      <div className="container">
+        <div className= "loginBox">
+          <div className = "title"> ANova Labs </div>
+          <form onSubmit={this._submit}>
+            <div>
+              <label for = "email">Email</label>
+              <input
+                type="text"
+                name="email"
+                onChange={this._change}
+                value={this.state.email}
+              />
+            </div>
+            <div>
+              <label for = "password">Password</label>
+              <input
+                type="text"
+                name="password"
+                onChange={this._change}
+                value={this.state.password}
+              />
+              <div>{this.state.errorMsg}</div>
+            </div>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
