f46554bd7a03acee15c7fa30c7049b5d7b903d25
JinsuElhance <33761390+JinsuElhance@users.noreply.github.com>
2019-03-22 15:10:15 -0700

    Added edit modal and formatted profile view

--- anovalabs_client/src/components/Profile.js
+++ anovalabs_client/src/components/Profile.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import './Profile.css';
-import { Button, Row, Col, Avatar } from 'antd';
-
+import { Modal, Input, Button, Row, Col, Avatar } from 'antd';
+import "antd/dist/antd.css";
 export default class Profile extends Component {
      state = {
           // profileimage: '../images/student.png',
@@ -10,58 +10,71 @@ export default class Profile extends Component {
           username: "John Smith",
           email: "potatofries@gmail.com",
           grade: "Senior",
-          bio: "Once upon a time"
+          bio: "Once upon a time",
+          candy: "Twix",
+          showEdit: false
      }
 
      constructor(props) {
           super(props)
-          this.handleClick = this.handleClick.bind(this);
-          this.changeUser = this.changeUser.bind(this);
-          this.changeBio = this.changeBio.bind(this);
-          this.changeEmail = this.changeEmail.bind(this);
-          this.changeGrade = this.changeGrade.bind(this);
-
+          // this.handleClick = this.handleClick.bind(this);
      }
-
-     handleClick(e) {
-          var editButton = e.currentTarget;
-          var editField = document.getElementById(editButton.id.slice(0,-4))
-          var userEdit = prompt("What would you like to change your " + editField.id+ " to?", editField.innerText)
-          let editFieldid = editField.id
-
+     handleChange(e) {
+          var editInput = e.currentTarget;
+          var inputText = e.currentTarget.value;
+          var editField = document.getElementById(editInput.id.slice(0, -5));
+          var editFieldid = editField.id;
           if (editFieldid == "username") {
-               this.changeUser(userEdit.valueOf())
+               this.changeUser(inputText.valueOf())
           } else if (editFieldid == "email") {
-               this.changeEmail(userEdit.valueOf())
+               this.changeEmail(inputText.valueOf())
           } else if (editFieldid == "bio") {
-               this.changeBio(userEdit.valueOf())
+               this.changeBio(inputText.valueOf())
           } else if (editFieldid == "grade") {
-               this.changeGrade(userEdit.valueOf())
+               this.changeGrade(inputText.valueOf())
           } else {
                alert("Something is amiss")
           }
      }
 
-     changeUser(newusername) {
-          this.setState(state => ({
-               username: newusername
-             }))
+     applyChanges() {
+          var userEdit = document.getElementById("userEdit");
+          var emailEdit = document.getElementById("emailEdit");
+          var gradeEdit = document.getElementById("gradeEdit");
+          var bioEdit = document.getElementById("bioEdit");
+          var candyEdit = document.getElementById("candyEdit");
+
+          this.setState({
+               username: userEdit.value,
+               email: emailEdit.value,
+               grade: gradeEdit.value,
+               bio: bioEdit.value,
+               candy: candyEdit.value
+          })
+
+          this.showModal(false)
      }
-     changeEmail(newemail) {
-          this.setState(state => ({
-               email: newemail
-             }))
+     showModal(bool) {
+          this.setState({ showEdit: bool });
      }
-     changeBio(newbio) {
-          this.setState(state => ({
-               bio: newbio
-             }))
-     }
-     changeGrade(newgrade) {
-          this.setState(state => ({
-               grade: newgrade
-             }))
-     }
+     // handleClick(e) {
+     //      var editButton = e.currentTarget;
+     //      var editField = document.getElementById(editButton.id.slice(0, -4))
+     //      // var userEdit = prompt("What would you like to change your " + editField.id+ " to?", editField.innerText)
+     //      let editFieldid = editField.id
+
+     //      if (editFieldid == "username") {
+     //           this.showUser()
+     //      } else if (editFieldid == "email") {
+     //           this.showEmail()
+     //      } else if (editFieldid == "bio") {
+     //           this.showBio()
+     //      } else if (editFieldid == "grade") {
+     //           this.showGrade()
+     //      } else {
+     //           alert("Something is amiss")
+     //      }
+     // }
 
      render() {
           return (
@@ -69,34 +82,83 @@ export default class Profile extends Component {
                     <div className="profileBox">
                          <Row type="flex">
                               <Col>
-                                   <Avatar src={this.state.profileimage} size="small" />
+                                   <Avatar style={{marginRight : "20px"}}src={this.state.profileimage} size="large" />
                               </Col>
                               <Col>
-                                   <h1 id = "username" className="title">{this.state.username}</h1>
+                                   <p id="username" className="title">{this.state.username}</p>
+                              </Col>
+                         </Row>
+                         <Row type="flex">
+                         <Col>
+                                   <p>Email:</p>
+                              </Col>
+                              <Col>
+                                   <p id="email">{this.state.email}</p>
+                              </Col>
+                         </Row>
+                         <Row type="flex">
+                         <Col>
+                                   <p>Grade:</p>
+                              </Col>
+                              <Col>
+                                   <p id="grade">{this.state.grade}</p>
+                              </Col>
+                         </Row>
+                         <Row type="flex">
+                         <Col>
+                                   <p>Candy:</p>
+                              </Col>
+                              <Col>
+                                   <p id="candy">{this.state.candy}</p>
                               </Col>
                          </Row>
                          <Row type="flex">
                               <Col>
-                                   <h1 id = "email" className="title">{this.state.email}</h1>
+                                   <p>Biography:</p>
                               </Col>
                               <Col>
-                              <Button className = "editButton" id = "emailedit" onClick = {(event) => this.handleClick(event)}><Avatar size={64} icon="edit" /></Button>
-                              </Col>
-                         </Row>
-                         <Row type="flex">
-                              <Col>
-                                   <h1 id = "grade" className="title">{this.state.grade}</h1>
-                              </Col>
-                              <Col>
-                              <Button className = "editButton" id = "gradeedit" onClick = {(event) => this.handleClick(event)}><Avatar size={64} icon="edit" /></Button>
+                                   <p id="bio">{this.state.bio}</p>
                               </Col>
                          </Row>
-                         <Row type="flex">
+                         <Row>
                               <Col>
-                                   <p id = "bio">{this.state.bio}</p>
-                              </Col>
-                              <Col>
-                                   <Button className = "editButton" id = "bioedit" onClick = {(event) => this.handleClick(event)}><Avatar size={64} icon="edit" /></Button>
+                                   <Avatar size={64} onClick={() => this.showModal(true)} icon="edit" />
+                                   <Modal
+                                        className="editModal"
+                                        title="Edit Your Profile"
+                                        centered
+                                        visible={this.state.showEdit}
+                                        onOk={() => this.applyChanges()}
+                                        onCancel={() => this.showModal(false)}
+                                   >
+                                        <div className="editFields">
+                                             <Row>
+                                                  <Col>
+                                                       <Input id="userEdit" allowClear={true} addonBefore="Username:" autosize={true} defaultValue={this.state.username}></Input>
+                                                  </Col>
+                                             </Row>
+                                             <Row>
+                                                  <Col>
+                                                       <Input id="emailEdit" type="email" allowClear={true} addonBefore="Email:" autosize="true" defaultValue={this.state.email}></Input>
+                                                  </Col>
+                                             </Row>
+                                             <Row>
+                                                  <Col>
+                                                       <Input id="gradeEdit" allowClear={true} addonBefore="Grade:" autosize="true" defaultValue={this.state.grade}></Input>
+                                                  </Col>
+                                             </Row>
+                                             <Row>
+                                                  <Col>
+                                                       <Input id="candyEdit" allowClear={true} addonBefore="Candy:" autosize="true" defaultValue={this.state.candy}></Input>
+                                                  </Col>
+                                             </Row>
+                                             <Row>
+                                                  <Col>
+                                                       <Input id="bioEdit" allowClear={true} addonBefore="Bio:" autosize="true" defaultValue={this.state.bio}></Input>
+                                                  </Col>
+                                             </Row>
+                                        </div>
+                                   </Modal>
                               </Col>
                          </Row>
                     </div>
