83525c34e351f2bbd2634a897e41af7ae1e08270
JinsuElhance <33761390+JinsuElhance@users.noreply.github.com>
2019-04-07 16:32:16 -0700

    Added Roster View

---
+++ anovalabs_client/src/components/Roster.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import './Roster.css';
+import ProfileCard from './ProfileCard'
+import { Icon, Card, Avatar, Col, Row } from 'antd';
+import "antd/dist/antd.css";
+
+
+var students = [
+     {
+          key : "1",
+          profileimage: "https://image.flaticon.com/icons/svg/1141/1141771.svg",
+          username: "John Smith",
+          email: "potatofries@gmail.com",
+          grade: "Senior",
+          bio: "Once upon a time",
+          candy: "Twix",
+     },
+     {
+          key : "2",
+          profileimage: "https://image.flaticon.com/icons/svg/1141/1141771.svg",
+          username: "John Smithson",
+          email: "potatopotato@gmail.com",
+          grade: "Freshman",
+          bio: "Once on time",
+          candy: "Candy",
+     },
+     {
+          key : "3",
+          profileimage: "https://image.flaticon.com/icons/svg/1141/1141771.svg",
+          username: "John Smithson",
+          email: "potatopotato@gmail.com",
+          grade: "Freshman",
+          bio: "Once on time",
+          candy: "Candy",
+     },
+     {
+          key : "4",
+          profileimage: "https://image.flaticon.com/icons/svg/1141/1141771.svg",
+          username: "John Smithson",
+          email: "potatopotato@gmail.com",
+          grade: "Freshman",
+          bio: "Once on time",
+          candy: "Candy",
+     },
+     {
+          key : "5",
+          profileimage: "https://image.flaticon.com/icons/svg/326/326933.svg",
+          username: "John Smithson",
+          email: "potatopotato@gmail.com",
+          grade: "Freshman",
+          bio: "Once on time",
+          candy: "Candy",
+     },
+     {
+          key : "2",
+          profileimage: "https://image.flaticon.com/icons/svg/1141/1141771.svg",
+          username: "John Smithson",
+          email: "potatopotato@gmail.com",
+          grade: "Freshman",
+          bio: "Once on time",
+          candy: "Candy",
+     },
+     {
+          key : "2",
+          profileimage: "https://image.flaticon.com/icons/svg/1141/1141771.svg",
+          username: "John Smithson",
+          email: "potatopotato@gmail.com",
+          grade: "Freshman",
+          bio: "Once on time",
+          candy: "Candy",
+     }
+]
+
+export default class Roster extends Component {
+     render() {
+          return (
+               <div className="container">
+               <div className="containerGrid">
+               {students.map((student) => <ProfileCard key = {student.key} student = {student}/>)}
+               </div>
+               </div>
+          )
+     }
+}
