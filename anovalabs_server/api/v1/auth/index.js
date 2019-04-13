c6635e443241ba5019a3dfceaa9eed85f4fcdb35
Christopher Cheung <cchrism0426@gmail.com>
2019-04-12 17:20:18 -0700

    deleted signup2 and login2

--- anovalabs_server/api/v1/auth/index.js
+++ anovalabs_server/api/v1/auth/index.js
@@ -33,7 +33,6 @@ function validatorAccount(account) {
 router.post('/signup', (req, res, next) => {
   const validAccount = validatorAccount(req.body);
   const accountId = uuidv4();
-  console.log("heyo")
   console.log(validAccount.error);
   if (validAccount.error === null) {
     User.getOneByEmail(req.body.email.trim()).then(user => {
@@ -67,38 +66,6 @@ router.post('/signup', (req, res, next) => {
   }
 });
 
-router.post('/signup2', (req, res, next) => {
-  const validAccount = validatorAccount(req.body);
-  console.log(validAccount.error);
-  if (validAccount.error === null) {
-    Account.getOneByEmail(req.body.email.trim()).then(account => {
-      if (!account) {
-        // technique #2 of bycrypt
-        bcrypt.hash(
-          req.body.password.trim(),
-          parseInt(process.env.SALT_ROUNDS, 10),
-          (err, hash) => {
-            // Store hash in your password DB.
-            const newAccount = {
-              email: req.body.email.trim(),
-              password: hash
-            };
-            Account.create(newAccount).then(accountId => {
-              res.json({
-                message: `defintely some #️⃣ ${accountId}`
-              });
-            });
-          }
-        );
-      } else {
-        next(new Error('Email in use'));
-      }
-    });
-  } else {
-    next(new Error('Invalid Account'));
-  }
-});
-
 router.post('/login', (req, res, next) => {
   const validAccount = validatorAccount(req.body);
   if (validAccount.error === null) {
@@ -108,47 +75,6 @@ router.post('/login', (req, res, next) => {
           if (result) {
             const payload = {
               email: user.email,
-              roles: 'this will be a list of roles from account_role table'
-            };
-            jwt.sign(
-              payload,
-              process.env.JWT_SECRET,
-              {
-                expiresIn: '2d'
-              },
-              (err, token) => {
-                if (err) {
-                  next(new Error('Invalid login'));
-                } else {
-                  res.json({
-                    token
-                  });
-                }
-              }
-            );
-          } else {
-            next(new Error('Invalid login'));
-          }
-        });
-      } else {
-        next(new Error('Invalid login'));
-      }
-    });
-  } else {
-    res.status(401);
-    next(new Error('Invalid Login'));
-  }
-});
-
-router.post('/login2', (req, res, next) => {
-  const validAccount = validatorAccount(req.body);
-  if (validAccount.error === null) {
-    Account.getOneByEmail(req.body.email.trim()).then(account => {
-      if (account) {
-        bcrypt.compare(req.body.password.trim(), account.password).then(result => {
-          if (result) {
-            const payload = {
-              email: account.email,
               roles: 'this will be a list of roles from account_role table'
             };
             jwt.sign(
--- anovalabs_server/temp/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const apiRoute = require('./api/v1/auth');
-const app = express();
-app.use('/api/v1/auth', apiRoute);
-
-// app.use('/api', apiRoute);
-app.listen('3000');
