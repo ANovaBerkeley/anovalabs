8fbc360f067e995039af8368613a5700a5a8c551
mluong888 <mluong888@berkeley.edu>
2019-04-13 18:58:33 -0700

    added gitignore?

--- anovalabs_server/newseedData/10_user_role_seed.js
+++ anovalabs_server/newseedData/10_user_role_seed.js
@@ -2,5 +2,13 @@ module.exports = [
   {
       user_id: 1,
       role_id: 1
+  },
+  {
+      user_id: 2,
+      role_id: 2
+  },
+  {
+      user_id: 3,
+      role_id: 3
   }
 ];
---
+++ anovalabs_server/newseedData/15_user_semester_site_seed.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+      semester_id: 1,
+      site_id: 1,
+      user_id:1
+  },
+  {
+      semester_id: 1,
+      site_id: 1,
+      user_id:2
+  },
+  {
+      semester_id: 1,
+      site_id: 1,
+      user_id:3
+  },
+  // {
+  //     semester_id: 2,
+  //     site_id: 2,
+  //     user_id:2
+  // },
+  // {
+  //     semester_id: 2,
+  //     site_id: 3,
+  //     user_id: 3
+  // }
+];
