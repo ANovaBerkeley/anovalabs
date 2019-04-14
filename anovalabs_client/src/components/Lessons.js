4dd781b59e65ff144690105419ef5377af2f3071
pratibha99 <pratibha99@berkeley.edu>
2019-04-14 10:33:15 -0700

    fixed styling

--- anovalabs_client/src/components/Lessons.js
+++ anovalabs_client/src/components/Lessons.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import LessonComponent from './LessonComponent';
 import MentorLessonComponent from './MentorLessonComponent';
-
+//
 import '../stylesheets/Lessons.css';
 import { GoPlus } from 'react-icons/go';
 
@@ -12,7 +12,7 @@ class Lessons extends Component {
     this.state = {
       error: null,
       isLoaded: true,
-      mentor: true,
+      mentor: false,
       items: [{"id":1,"title":"Python 1 for inexperienced","summary":"1st Python lesson","link":"https://docs.google.com/presentation/u/2/d/1Ow8eswXrAmz6TGTJs3C5l0kxNubV5PFVy0xVIRm5SLA/edit?usp=drive_web&ouid=107773852241053411405","created_at":"2019-03-11T01:40:56.187Z","updated_at":"2019-03-11T01:40:56.187Z", "date": "3/11"}, {"id":1,"title":"Python 1 for inexperienced","summary":"1st Python lesson","link":"https://docs.google.com/presentation/u/2/d/1Ow8eswXrAmz6TGTJs3C5l0kxNubV5PFVy0xVIRm5SLA/edit?usp=drive_web&ouid=107773852241053411405","created_at":"2019-03-11T01:40:56.187Z","updated_at":"2019-03-11T01:40:56.187Z", "date": "3/11"}, {"id":1,"title":"Python 1 for inexperienced","summary":"1st Python lesson","link":"https://docs.google.com/presentation/u/2/d/1Ow8eswXrAmz6TGTJs3C5l0kxNubV5PFVy0xVIRm5SLA/edit?usp=drive_web&ouid=107773852241053411405","created_at":"2019-03-11T01:40:56.187Z","updated_at":"2019-03-11T01:40:56.187Z", "date": "3/11"}, {"id":1,"title":"Python 1 for inexperienced","summary":"1st Python lesson","link":"https://docs.google.com/presentation/u/2/d/1Ow8eswXrAmz6TGTJs3C5l0kxNubV5PFVy0xVIRm5SLA/edit?usp=drive_web&ouid=107773852241053411405","created_at":"2019-03-11T01:40:56.187Z","updated_at":"2019-03-11T01:40:56.187Z", "date": "3/11"}, {"id":1,"title":"Python 1 for inexperienced","summary":"1st Python lesson","link":"https://docs.google.com/presentation/u/2/d/1Ow8eswXrAmz6TGTJs3C5l0kxNubV5PFVy0xVIRm5SLA/edit?usp=drive_web&ouid=107773852241053411405","created_at":"2019-03-11T01:40:56.187Z","updated_at":"2019-03-11T01:40:56.187Z", "date": "3/11"}
     ]
     };
@@ -68,16 +68,13 @@ class Lessons extends Component {
               <LessonComponent lessonDetails={item}></LessonComponent>
             ))}
 
-            <div className = "plusCard">
-              <GoPlus size = {100} color='grey'/>
-            </div>
+
 
 
           </div>
         </div>
       );
     } else {
-      // return <h3> Lets go for a < GoPlus/>? </h3>
       return (
         <div className = "container">
           <div className = "lessonsContainer">
