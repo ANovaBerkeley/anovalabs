588faefb1a943dff65c5c4aaf6861b753f59f9e1
Christopher Franco <francogments@gmail.com>
2020-03-19 08:40:28 -0700

    deploy & test

--- controllers/lessons_controller.js
+++ controllers/lessons_controller.js
@@ -16,17 +16,18 @@ const index = async (req, res, next) => {
 /* Add a lesson to the lesson pool. */
 const create = async (req, res, next) => {
   try {
-    const data = await knex('lesson').insert(req.body);
-    return res.status(201).send(data);
+    await knex('lesson').insert(req.body);
+    return res.status(201).send({ title: req.body.title });
   } catch (error) {
     return res.status(500).json({ error });
   }
 };
 /* Update a lesson details */
 const update = async (req, res, next) => {
+  const { lessonId } = req.body;
   try {
-    const data = await nex('lesson')
-      .where({ id: req.body.lessonId })
+    const data = await knex('lesson')
+      .where({ id: lessonId })
       .update({
         title: req.body.editedTitle,
         summary: req.body.editedSummary,
@@ -38,16 +39,16 @@ const update = async (req, res, next) => {
   }
 };
 /* Delete a lesson from the lesson pool. */
-const deleteLesson = (req, res, next) => {
-  knex('lesson')
-    .where({ id: req.body.id })
-    .del()
-    .then(data => {
-      res.status(200).send({ data });
-    })
-    .catch(error => {
-      res.status(500).json({ error });
-    });
+const deleteLesson = async (req, res, next) => {
+  const { id } = req.body;
+  try {
+    const data = await knex('lesson')
+      .where({ id: id })
+      .del();
+    return res.status(200).send({ data });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 
 module.exports = {
--- controllers/profiles_controller.js
+++ controllers/profiles_controller.js
@@ -1,36 +1,40 @@
 const knex = require('../db/knex');
 
 /* Retrieve a user's profile based on their id. */
-const getProfileById = (req, res, next) => {
+const getProfileById = async (req, res, next) => {
   const userid = req.query.uid;
-
-  knex
-    .select(
-      'user.email',
-      'user.picture',
-      'user.candy',
-      'user.name',
-      'user.hobby',
-      'user.notes',
-      'user.role',
-    )
-    .from('user')
-    .where('user.id', userid)
-    .then(data => {
-      return res.status(200).send(data);
-    });
+  try {
+    const data = await knex
+      .select(
+        'user.email',
+        'user.picture',
+        'user.candy',
+        'user.name',
+        'user.hobby',
+        'user.notes',
+        'user.role',
+      )
+      .from('user')
+      .where('user.id', userid);
+    return res.status(200).send(data);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 /* Update a user's profile. */
-const update = (req, res, next) => {
-  knex('user')
-    .where({ id: req.body.id })
-    .update({ hobby: req.body.hobby, candy: req.body.candy, notes: req.body.notes })
-    .then(data => {
-      res.status(200).send({ data });
-    })
-    .catch(error => {
-      res.status(500).json({ error });
-    });
+const update = async (req, res, next) => {
+  try {
+    const data = await knex('user')
+      .where({ id: req.body.id })
+      .update({
+        hobby: req.body.hobby,
+        candy: req.body.candy,
+        notes: req.body.notes,
+      });
+    return res.status(200).send({ data });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 
 module.exports = {
