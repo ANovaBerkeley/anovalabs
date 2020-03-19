622274af7c919fce9a60b6fec7a74a9402e54afd
Christopher Franco <francogments@gmail.com>
2020-03-19 06:32:13 -0700

    try and catch to fulfill promises

--- controllers/rosters_controller.js
+++ controllers/rosters_controller.js
@@ -1,36 +1,35 @@
 const knex = require('../db/knex');
 
 /* Retrieve the list of students for a specific site. */
-const getUsersBySite = (req, res) => {
+const getUsersBySite = async (req, res) => {
   const userid = req.query.uid;
   const roleType = req.query.roleToRetrieve;
-
-  const siteid = knex
-    .select('site_id')
-    .from('user_semester_site')
-    .where('user_semester_site.user_id', userid);
-
-  knex
-    .select('user.id', 'name', 'email', 'picture', 'notes')
-    .from('user_semester_site')
-    .rightJoin('user', 'user.id', 'user_semester_site.user_id')
-    .where('site_id', siteid)
-    .where('role', roleType)
-    .then(data => {
-      res.status(200).send(data);
-    });
+  try {
+    const siteid = await knex
+      .select('site_id')
+      .from('user_semester_site')
+      .where('user_semester_site.user_id', userid);
+    const data = await knex
+      .select('user.id', 'name', 'email', 'picture', 'notes')
+      .from('user_semester_site')
+      .rightJoin('user', 'user.id', 'user_semester_site.user_id')
+      .where('site_id', siteid[0].site_id)
+      .where('role', roleType);
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 /* Update a specific student profile */
-const update = (req, res) => {
-  knex('user')
-    .where({ id: req.body.userId })
-    .update({ notes: req.body.editedNotes })
-    .then(data => {
-      res.status(200).send({ data });
-    })
-    .catch(error => {
-      res.status(500).json({ error });
-    });
+const update = async (req, res) => {
+  try {
+    const data = await knex('user')
+      .where({ id: req.body.userId })
+      .update({ notes: req.body.editedNotes });
+    return res.status(200).send({ data });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 module.exports = {
--- controllers/sites_controller.js
+++ controllers/sites_controller.js
@@ -1,50 +1,49 @@
 const knex = require('../db/knex');
 
 /* Returns all site names */
-const index = (req, res, next) => {
-  knex
-    .select('schoolName', 'id')
-    .from('site')
-    .then(data => {
-      res.send(data);
-    });
+const index = async (req, res, next) => {
+  try {
+    const data = await knex.select('schoolName', 'id').from('site');
+    return res.send(data);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 /* Returns the current site of the user. */
-const getCurrentUserSite = (req, res, next) => {
+const getCurrentUserSite = async (req, res, next) => {
   const userid = req.query.uid;
+  try {
+    const siteid = await knex
+      .select('site_id')
+      .from('user_semester_site')
+      .where('user_semester_site.user_id', userid);
 
-  const siteid = knex
-    .select('site_id')
-    .from('user_semester_site')
-    .where('user_semester_site.user_id', userid);
-
-  knex
-    .select('site.schoolName')
-    .from('site')
-    .where('site.id', siteid)
-    .first()
-    .then(data => {
-      res.send(data);
-    });
+    const data = await knex
+      .select('site.schoolName')
+      .from('site')
+      .where('site.id', siteid[0].site_id)
+      .first();
+    return res.send(data);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 /* Add user to a site + semester. */
-const addUserToSemSite = (req, res, next) => {
-  knex('user_semester_site')
-    .insert({
+const addUserToSemSite = async (req, res, next) => {
+  try {
+    await knex('user_semester_site').insert({
       user_id: req.body.user_id,
       semester: req.body.semester,
       site_id: req.body.site_id,
-    })
-    .then(() => {
-      res.status(201).json({
-        user_id: req.body.user_id,
-        semester: req.body.semester,
-        site_id: req.body.site_id,
-      });
-    })
-    .catch(error => {
-      res.status(500).json({ error });
     });
+    return res.status(201).json({
+      user_id: req.body.user_id,
+      semester: req.body.semester,
+      site_id: req.body.site_id,
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 };
 
 module.exports = {
