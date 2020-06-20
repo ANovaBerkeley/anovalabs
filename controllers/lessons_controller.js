a6532a7e770b8e4579852ecefce23cb2f7734547
Robin Qiu <rqiu@berkeley.edu>
2020-06-20 14:48:06 -0700

    feedback migration, controller, api calls

--- controllers/lessons_controller.js
+++ controllers/lessons_controller.js
@@ -102,20 +102,20 @@ const getFeedback = async (req, res, next) => {
 
 }
 
-/* Submit mentor feedback for a lesson. */
-const submitFeedback = async (req, res, next) => {
-  const { lessonId } = req.body;
-  try {
-    const data = await knex('lesson')
-      .where({ id: lessonId })
-      .update({
-        feedback: req.body.feedback,
-      });
-    return res.status(200).send({ data });
-  } catch (error) {
-    return res.status(500).json({ error });
-  }
-}
+// /* Submit mentor feedback for a lesson. */
+// const submitFeedback = async (req, res, next) => {
+//   const { lessonId } = req.body;
+//   try {
+//     const data = await knex('lesson')
+//       .where({ id: lessonId })
+//       .update({
+//         feedback: req.body.feedback,
+//       });
+//     return res.status(200).send({ data });
+//   } catch (error) {
+//     return res.status(500).json({ error });
+//   }
+// }
 
 module.exports = {
   index: index,
