7be23d12619cc7a14cf491bf8218758fe2292190
Kenny Chi <kennychi@berkeley.edu>
2020-10-18 20:05:20 -0700

    fixed name error in Feedback.js

--- controllers/lessons_controller.js
+++ controllers/lessons_controller.js
@@ -66,34 +66,34 @@ const deleteLesson = async (req, res, next) => {
   }
 };
 
-// /* Retrieve mentor feedback for a lesson. */
-// const getFeedback = async (req, res, next) => {
-//   const lessonId = req.query.lessonId;
-//   try {
-//     const data = await knex
-//       .select('lesson.mentor_feedback')
-//       .where({ id: lessonId })
-//       .from('lesson');
-//     return res.status(200).send({ data });
-//   } catch (error) {
-//     return res.status(500).json({ error});
-//   }
-// }
+/* Retrieve mentor feedback for a lesson. */
+const getFeedback = async (req, res, next) => {
+  const lessonId = req.query.lessonId;
+  try {
+    const data = await knex
+      .select('lesson.mentor_feedback')
+      .where({ id: lessonId })
+      .from('lesson');
+    return res.status(200).send({ data });
+  } catch (error) {
+    return res.status(500).json({ error});
+  }
+}
 
-// /* Submit mentor feedback for a lesson. */
-// const submitFeedback = async (req, res, next) => {
-//   const { lessonId } = req.body;
-//   try {
-//     const data = await knex('lesson')
-//       .where({ id: lessonId })
-//       .update({
-//         feedback: req.body.feedback,
-//       });
-//     return res.status(200).send({ data });
-//   } catch (error) {
-//     return res.status(500).json({ error });
-//   }
-// }
+/* Submit mentor feedback for a lesson. */
+const submitFeedback = async (req, res, next) => {
+  const { lessonId } = req.body;
+  try {
+    const data = await knex('lesson')
+      .where({ id: lessonId })
+      .update({
+        feedback: req.body.feedback,
+      });
+    return res.status(200).send({ data });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+}
 
 module.exports = {
   index: index,
