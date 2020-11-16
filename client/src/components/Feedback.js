6e8ce08638643a7e21cd082d8b290190d6102d99
Joy <joydong@berkeley.edu>
2020-11-15 19:27:57 -0800

    Some starter changes on feedback page

--- client/src/components/Feedback.js
+++ client/src/components/Feedback.js
@@ -3,73 +3,103 @@ import { Popconfirm, Button, Modal, Row, Col, Input } from 'antd';
 import { GoTrashcan } from 'react-icons/go';
 import PropTypes from 'prop-types';
 import * as decode from 'jwt-decode';
+import '../stylesheets/FeedbackPage.css';
 
 class Feedback extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            studentRating: null,
-            mentorRating: null,
-            notes: null,
-            lessonName: null,
-            isMentor: this.props.isMentor,
-            lessonID: this.props.match.params.id
-        }
-    }
-    componentDidMount() {
-        var get_url = '/api/v1/lessons/get_feedback/';
-        fetch(get_url + '?lessonId='+ this.lessonId)
-            .then(res => res.json())
-            .then(
-                feedback => {
-                    this.setState({
-                        isLoaded: true,
-                        notes:  feedback.mentor_feedback,
-                     });
-                },
-                error => {
-                    this.setState({
-                        isLoaded: true,
-                        error,
-                    });
-                },
-            );
-    }
-
-    editFeedback() {
-        var get_url = '/api/v1/lessons/submit_feedback/'
-    }
-
-    updateFeedback() {
-      const { editedText, editedRating, uid, lid } = this.state;
-
-      fetch('/api/v1/feedback/update', {
-        method: 'POST',
-        body: JSON.stringify({
-          updatedText,
-          updatedRating,
-          userId,
-          lessonId,
-        }),
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        }),
-      })
-        .then(res => res.json())
-        .then(values => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // userId: this.props.person.id,
+      lessonId: this.props.matchparam,
+      text: null,
+      rating: null,
+      isMentor: this.props.isMentor,
+    };
+  }
+  componentDidMount() {
+    var get_url = '/api/v1/lessons/get_feedback/';
+    fetch(get_url + '?lessonId=' + this.lessonId)
+      .then(res => res.json())
+      .then(
+        feedback => {
           this.setState({
-            showEditModal: false,
-            text: updatedText,
-            rating: updatedRating
+            isLoaded: true,
+            notes: feedback.mentor_feedback,
           });
-        });
-    }
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        },
+      );
+  }
 
-    render() {
-        return (
-            <div></div>
-        );
-    }
+  submitFeedback() {
+    var get_url = '/api/v1/lessons/submit_feedback/';
+  }
+
+  updateFeedback() {
+    const { editedText, editedRating, uid, lid } = this.state;
+
+    fetch('/api/v1/feedback/update', {
+      method: 'POST',
+      body: JSON.stringify({
+        editedText,
+        editedRating,
+        uid,
+        lid,
+      }),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+    })
+      .then(res => res.json())
+      .then(values => {
+        this.setState({
+          showEditModal: false,
+          text: editedText,
+          rating: editedRating,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="page">
+        <div className="feedbackBoxContainer">
+          <h3>What did you think of today's lesson?</h3>
+          <input className="feedbackInput"></input>
+        </div>
+        <div className="ratingContainer">
+          <h3>Rate today's lesson on a scale from 1-5!</h3>
+          <button className="rate" onClick={() => (this.rating = 1)} type="button">
+            1
+          </button>
+          <button className="rate" onClick={() => (this.rating = 2)} type="button">
+            2
+          </button>
+          <button className="rate" onClick={() => (this.rating = 3)} type="button">
+            3
+          </button>
+          <button className="rate" onClick={() => (this.rating = 4)} type="button">
+            4
+          </button>
+          <button className="rate" onClick={() => (this.rating = 5)} type="button">
+            5
+          </button>
+        </div>
+        <button
+          className="submitButton"
+          onClick={() => this.submitFeedback()}
+          type="button"
+        >
+          Submit Feedback
+        </button>
+      </div>
+    );
+  }
 }
 
 Feedback.defaultProps = {};
