08722e9f6576cfd23237a5523236d75d375d94c4
Jimmy Lee <jimmyminhlee@gmail.com>
2021-04-18 15:21:13 -0700

    Moved everything into a function

--- client/src/components/ReplitiFrame.js
+++ client/src/components/ReplitiFrame.js
@@ -1,47 +1,48 @@
 import { Input } from 'antd';
 
-// props.editMode
-// props.replitLink
-//
-
-// if props.editMode => return
-// if replit =>
 const ReplitiFrame = (props) => {
   const {editMode, replit, onChangeReplit} = props;
-  let maybeReplit;
-  if (editMode) {
-    maybeReplit = (
-      <div>
-        <h2 className="textTitle"> Examples </h2>
-        <div className="textBox">
-          <Input
-            id="replitAdd"
-            allowClear
-            addonBefore="Replit link:"
-            autosize="true"
-            defaultValue={ replit || "https://replit.com/@anova/..." }
-            onChange={onChangeReplit}
+
+  /**
+      If we're in editMode, we'll return the input field that asks for a replit link.
+      Otherwise, if we have a replit link, we'll return the iFrame with the Replit.
+  */
+
+  const renderReplit = () => {
+    if (editMode) {
+      return (
+        <div>
+          <h2 className="textTitle"> Examples </h2>
+          <div className="textBox">
+            <Input
+              id="replitAdd"
+              allowClear
+              addonBefore="Replit link:"
+              autosize="true"
+              defaultValue={ replit || "https://replit.com/@anova/..." }
+              onChange={onChangeReplit}
+            />
+          </div>
+        </div>
+      );
+    }
+    else if (replit) {
+      return (
+        <div>
+          <h2 className="textTitle"> Examples </h2>
+          <iframe
+            title="Replit example"
+            className="textBox"
+            width="100%"
+            height="500px"
+            src={replit.concat("?lite=true")}
           />
         </div>
-      </div>
-    );
+      );
+    }
   }
-  else if (replit) {
-    maybeReplit = (
-      <div>
-        <h2 className="textTitle"> Examples </h2>
-        <iframe
-          title="Replit example"
-          className="textBox"
-          width="100%"
-          height="500px"
-          src={replit.concat("?lite=true")}
-        />
-      </div>
-    );
-  }
-
-  return <div> {maybeReplit} </div>;
+  /** Return whatever renderReplit gives us */
+  return <div> {renderReplit()} </div>;
 }
 
 export default ReplitiFrame;
